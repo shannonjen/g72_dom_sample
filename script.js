4ca700fa4d636fdb89c162b024efbe89a00283f3
Jen Shannon <jennifer.shannon@galvanize.com>
2017-11-13 12:00:30 -0500

    Add interactivity

---
+++ script.js
@@ -0,0 +1,5 @@
+
+
+document.querySelector('p').addEventListener('click', function(){
+  document.querySelector('.fun').innerHTML = "<img src='https://harmoniaphilosophica.files.wordpress.com/2015/08/0e3df60cabeec611be2872b82db57458.jpg' alt='unicorn pic'>"
+})
